32dd4dd5f6e1a578fac3d7c1b4e919dbd00ef37b
Neil <nilnil47@gmail.com>
2018-01-24 00:28:22 +0200

    add the stage module

---
+++ stage.py
@@ -0,0 +1,91 @@
+import pygame as pg 
+
+class Stage:
+    
+    def __init__(self, background, high, width):
+        self.background = pg.image.load(background)
+        self.object_list = []
+
+        self.high = high
+        self.width = width
+        self.game_display = pg.display.set_mode((self.high, self.width))
+
+        self.add_object(self)
+
+    def add_object(self, obj):
+        """
+        add object to the object list 
+        """
+        self.object_list.append(obj)
+
+    def display_objects(self):
+        """
+        display all the objects in the stage on the screem 
+        """
+        for obj in self.object_list:
+            self.game_display.blit(obj.get_image(), obj.get_pos())
+        pg.display.flip()
+
+    def get_pos(self):
+        """
+        return the position of the object
+        """
+        return 0, 0
+
+    def get_image(self):
+        """
+        return the image of the object
+        """
+        return self.background
+
+class Platforme(pg.sprite.Sprite):
+    
+    def __init__(self, stage, image, start_pos):
+        stage.add_object(self) # add the monster to the relevant stage
+        self.image = pg.image.load(image)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = start_pos
+    
+    def get_pos(self):
+        """
+        return the position of the object
+        """
+        return self.rect.x, self.rect.y
+
+    def get_image(self):
+        return self.image
+
+class Monster(Platforme):
+    pass
+
+
+def main():
+    """
+    test function
+    """
+
+    pg.init()
+
+    IMAGES_FOLDER = './assets/'
+    BACKGROUND = IMAGES_FOLDER + 'bkgd.png'
+    PLATFORME_IMAGE = IMAGES_FOLDER + 'player.png'
+
+    HIGH = 600
+    WIDTH = 800
+    FPS = 60
+
+    clock = pg.time.Clock()
+    st = Stage(BACKGROUND, HIGH, WIDTH)
+    mo = Platforme(st, MONSTER_IMAGE, (50, 50))
+
+    st.display_objects()
+
+    while True:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+        clock.tick(FPS)
+
+if __name__ == '__main__':
+    main()
