a34c394931006fb213cf037561ac529d167fca65
galnil <zafargal@gmail.com>
2018-01-26 16:54:02 +0200

    made changes to the player movement --> moves smoothly

--- main.py
+++ main.py
@@ -15,12 +15,19 @@ FPS = 60
 white = (255, 255, 255, 255)
 black = (0, 0, 0, 255)
 
+pg.key.set_repeat(10,10) # the get_repeat method sends multiple pygame events while a key is being held down
 
+BOOL = pg.event.get_grab()
+if BOOL:
+	print('recieving keyboard input')
+else:
+	print(BOOL) # --> 0
 
 assets_wd = './assets/'
 # importing images
-# most bottom platform on the screen
+		# most bottom platform on the screen
 ms_bk = 'bkgd.png' # 0,520 --> coordinates
+
 bk_path = assets_wd + ms_bk
 bkgd = pg.image.load(bk_path).convert_alpha()
 # floating platforms
@@ -33,17 +40,28 @@ platform = pg.image.load(path_platform).convert_alpha()
 
 object_group = pg.sprite.Group() # to add to this group --> object_group.add(object)
 
-def events(e):
+def events(player):
 	#print ('events called')
 
 	""" handles keyboard, exit events """
-	#while True
-	if e.type == pg.QUIT:
-		print('exit called')
-		pg.quit()
-		sys.exit(1)
 
+	for e in pg.event.get():
+		if e.type == pg.QUIT:
+			print('exit called')
+			pg.quit()
+			sys.exit(1)
+		if e.type == pg.KEYDOWN:
+				# call the move player function
 
+				event_name = pg.key.name(e.key)
+				player.move(event_name)
+
+				#print('events fuc e.key: {0}'.format(e.key))
+"""
+		if e.type == pg.KEYUP:
+			player.key_up()
+			print('pg.KEYUP CALLED')
+"""
 class Player(pg.sprite.Sprite):
 	"""
 	This Player class defines the movement and actions of the Player
@@ -53,7 +71,7 @@ class Player(pg.sprite.Sprite):
 	"""
 	def __init__(self, width, height, img):
 		"""
-		defining the basic featues of the Player class
+		defining the basic features of the Player class
 		args:
 			width - the width of the player rectangle
 			height - the height of the player rectangle
@@ -61,10 +79,16 @@ class Player(pg.sprite.Sprite):
 			img - the sprite image of the player
 		
 		return value:
-			None
+	                 None
 		"""
+                # setting the Player's height, and width
 		self._w = width 
-		self._h = width
+		self._h = height
+                
+                # image configurations
+		self.image = pg.image.load(img)
+		self.image = pg.transform.scale(self.image, (self._w, self._h))
+		self.rect = self.image.get_rect(center=(100,100))
 		self.rect.x = 0
 		self.rect.y = 300
 		self._SPEED = 3
@@ -73,125 +97,122 @@ class Player(pg.sprite.Sprite):
 		self._x_change = 0
 		self._y_change = 0
 
-		# image configurations
-		self.image = pg.image.load(img)
-		self.image = pg.transform.image(self._w, self._h)
-		self.rect = pg.get_rect(self.image)
 	def _show(self):
 		"""
 		print the rect player to the script at location x,y, width, 
 		height = self._w, self._h
 		"""
 		#print ('showing rect')
-		#self._x += self._x_change
+                #self._x += self._x_change
 		#self._y += self._y_change
-		rect = pg.draw.rect(gameDisplay, self._c, [self._x, self._y, self._w, self._h])
+		#rect = pg.draw.rect(gameDisplay, white, [self.rect.x, self.rect.y, self._w, self._h])
+		gameDisplay.blit(self.image, (self.rect.x,self.rect.y))
+
 	
+	def key_up(self):
+		self._x_change = 0
+		self._y_change = 0
+		self._x_direction = 0
+		self._y_direction = 0
+
 	def _movement(self):
 		print('movement called')
 
-		for e in pg.event.get():
-			#mouse = pg.mouse.get_pos()
-			#print(mouse)
+		"""
 			if e.type == pg.QUIT:
 				events(e.type)
 			if e.type == pg.KEYDOWN:
 				if e.key == pg.K_RIGHT:
 					self._x_change += self._SPEED
 					self.x_direction = 1
-				
+					print('pressed right')
+					self.key_up()
 				if e.key == pg.K_LEFT:
 					self._x_change -= self._SPEED
 					self_x_direction = -1
-
-				
+					print('pressed left')
+					self.key_up()
 				if e.key == pg.K_UP:
 					self._y_change -= self._SPEED
 					self._y_direction = -1
-					print('pressed up ')
-				
+					print('pressed up')
+					self.key_up()
 				if e.key == pg.K_DOWN:
 					self._y_change += self._SPEED
 					self._y_direction = 1
-					
+					print('pressed down')
+					self.key_up()
+			
 			if e.type == pg.KEYUP:
-				if e.key in ['K_RIGHT, K_LEFT, K_UP, K_DOWN']:
-				#if e.key == 'K_RIGHT' or e.key == 'K_LEFT' or e.key == 'K_UP' or e.key == 'K_DOWN':
-					self._x_change = 0
-					self._y_change = 0
-					self._x_direction = 0 
-					self._y_direction = 0
+				#if e.key in ['K_RIGHT, K_LEFT, K_UP, K_DOWN']:
+				if e.key == 'K_RIGHT' or e.key == 'K_LEFT' or e.key == 'K_UP' or e.key == 'K_DOWN':
+					self.key_up()
 				#print('Player x,y: {0} {1}').format(self._x,self._y)
+				"""
 		"""
 		handling the player movement
 		"""
-		pass
 	
-	def _move(self, direction):
+	def move(self, event_name):
+		# pygame.event.event_name(type) --> the event name as string
 		"""
 		the move functions handles the player movement.
 		args:
-			direction - indicates the direction the player should be moved to
+			event_key - the event key indicates the name of the key pressed by the user
 		"""
-		pass
-		
+		moves = {'right': ('x', 1), 'left': ('x', -1), 'up': ('y', -1), 'down':('y', 1)} # RIGHT, LEFT, UP, DOWN
+		# k = keyboard key, v = (axis_direction, sign)
+		if event_name in moves:
+			"move the player to the direction indicated by the event_key"
+			axis, sign = moves[event_name]
+
+			if axis == 'x':
+				self._x_change += sign*self._SPEED
+				self._x_direction = sign
+				
+			if axis == 'y':
+				self._y_change += sign*self._SPEED
+				self._y_direction = sign
+			
+			self._update()
+			self.key_up()
 		
 	def _update(self):
-		print ('update called')
-		#try:
-		self._movement()
-		#except Exception as e:
-			#print('self.movement func error')
-		#finally:
 
-		self._x += self._x_change
-		self._y += self._y_change
-		print(self._x)
-		print(self._y)
-		
+		self.rect.x += self._x_change
+		self.rect.y += self._y_change
+		"""
+		print('__update__')
+		print('x after addition: {0}'.format(self.rect.x))
+		print('y after addition: {0}'.format(self.rect.y))
+		"""
 
 def main():
 	print ('main game')
-	try:
-		p = Player(50, 50, 'assets/player.png')
-	except Exception as e:
-		print ('could not initialize player')
+        
+	#try:
+	p = Player(100, 100, 'assets/player.png')
+	#except Exception as e:
+	#print ('could not initialize player')
 		
 	running = True
 	
 	while running:
 		#print ('running!')
-		gameDisplay.fill(white)
+		gameDisplay.fill(black)
 		#pdb.set_trace()
 		gameDisplay.blit(bkgd,(0,205))
 		gameDisplay.blit(platform, (200, 350))
 		p._show()
-		#try:
-		p._update()
-			
-		for e in pg.event.get():
-				#p._movement()
-			if e.type == pg.QUIT:
-				pg.quit()
-				sys.exit(1)
-				"""
-				if e.type == pg.KEYDOWN:
-					if e.key == pg.K_RIGHT:
-						p._x_change += p._x_speed
-					if e.key == pg.K_LEFT:
-						p._x -= p._x_speed
-					if e.key == pg.K_UP:
-						p._y -= p._y_speed
-					if e.key == pg.K_DOWN:
-						p._y += p._y_speed
-			
-				"""
-			
-			if p._y < 500 and p._y + p._SPEED > 500:
-				p._y = 499
-				#print (p._y)
-			else: 
-				p._y += p._SPEED
+		#p._update()
+		events(p)
+		"""
+		if p.rect.y < 500 and p.rect.y + p._SPEED > 500:
+			p.rect.y = 499
+			#print (p._y)
+		else:
+			#p.rect.y += p._SPEED
+			pass
 			
 		
 		#except Exception as e:
